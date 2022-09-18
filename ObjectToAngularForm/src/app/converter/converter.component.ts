bc246612f4743b12b6611965fca7f0c48478dfa7
Lucjan Tronina <tronina@technika.gliwice.pl>
2022-09-18 20:15:36 +0200

    before result creation

--- ObjectToAngularForm/src/app/converter/converter.component.ts
+++ ObjectToAngularForm/src/app/converter/converter.component.ts
@@ -1,16 +1,63 @@
 import { Component, OnInit } from '@angular/core';
 
-
 @Component({
   selector: 'con-converter',
   templateUrl: './converter.component.html',
   styleUrls: ['./converter.component.scss']
 })
 export class ConverterComponent implements OnInit {
+  public sampleForm: any = { name: 'Johny', surname: 'Bravo', sex: 'male' };
+  public config: { [name: string]: configType } = {};
+  public error: string = '';
+  public configTypeEnum = configType;
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
+    this.refreshConfig();
   }
 
+  buildForm(){
+
+  }
+
+
+  objectChange(input: any) {
+    if (!this.checkIsObject(input)) {
+      const msg = 'Not a object.';
+      this.error = msg;
+      console.log(msg);
+      return;
+    }
+    this.sampleForm = JSON.parse(input);
+    this.refreshConfig();
+  }
+
+  private refreshConfig() {
+    this.error = '';
+    this.config = {};
+
+    if (this.sampleForm == null) {
+      this.error = 'no data';
+      return;
+    }
+
+    for (const iterator in this.sampleForm) {
+      this.config[iterator] = configType.property;
+    }
+  }
+
+
+  private checkIsObject(input: any) {
+    try {
+      JSON.parse(input);
+    } catch (error) {
+      return false;
+    }
+    return true;
+  }
+}
+enum configType {
+  property = 'property',
+  option = 'option',
 }
--- ObjectToAngularForm/src/app/shared/shared.module.ts
+++ ObjectToAngularForm/src/app/shared/shared.module.ts
@@ -1,16 +1,38 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
 import { MatExpansionModule } from '@angular/material/expansion';
 import { CdkAccordionModule } from '@angular/cdk/accordion';
 import { MatTabsModule } from '@angular/material/tabs';
-import {MatCardModule} from '@angular/material/card';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
+import { MatButtonModule } from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon'
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule, MatExpansionModule, CdkAccordionModule, MatTabsModule,MatCardModule
-
+    CommonModule,
+    MatExpansionModule,
+    CdkAccordionModule,
+    MatTabsModule,
+    MatCardModule,
+    MatFormFieldModule,
+    FormsModule,
+    MatSelectModule,
+    MatButtonModule,MatIconModule
   ],
-  exports: [MatExpansionModule, CdkAccordionModule, MatTabsModule,MatCardModule]
+  exports: [
+    MatExpansionModule,
+    CdkAccordionModule,
+    MatTabsModule,
+    MatCardModule,
+    MatFormFieldModule,
+    FormsModule,
+    MatSelectModule,
+    MatButtonModule,MatIconModule
+  ],
 })
-export class SharedModule { }
+export class SharedModule {}
